25ba2505dd3c0b41149ea534f72f4952e9bbf28a
Services Team <services@sonarsource.com>
2023-06-21 09:31:53 +0200

    initial commit

---
+++ src/main/java/com/sonarsource/springdemo/AppController.java
@@ -0,0 +1,25 @@
+package com.sonarsource.springdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AppController {
+
+    @Autowired
+    DataUtils du;
+
+	@RequestMapping("/")
+	public @ResponseBody String greeting() {
+		return "Hello, World";
+	}
+
+    @RequestMapping("/person/address")
+    public @ResponseBody String getAddress(@RequestParam String name) {
+        return du.getAddress(name);
+    }
+
+}
